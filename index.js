85d50db24e510978280df78d95b368ced2ef2896
Paulo <pauloestrella1994@gmail.com>
2020-11-29 19:08:00 -0300

    Ignore node_modules and add first page js

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var prom = require('prom-client');
+
+var app = express()
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000);
