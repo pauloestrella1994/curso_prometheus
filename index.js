8f2d50a30e4a67df3ab4978bc7931901f36d9828
Paulo <pauloestrella1994@gmail.com>
2020-11-29 19:36:34 -0300

    Add metric to status 200

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var prom = require('prom-client');
+const register = prom.register;
 
 var app = express()
 
+const client = require('prom-client');
+const counter = new client.Counter({
+    name: 'request_total',
+    help: 'request counter',
+    labelNames: ['statusCode']
+})
+
 app.get('/', function(req, res) {
+    counter.labels('200').inc();
+
     res.send('Hello World!');
 });
+
+app.get('/metrics', function(req, res) {
+    res.set('Content-Type', register.contentType);
+    res.end(register.metrics());
+})
 
 app.listen(3000);
