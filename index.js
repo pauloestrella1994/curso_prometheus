140e9971db33ec66183a597d675e5382cb28d356
Paulo <pauloestrella1994@gmail.com>
2020-11-29 21:15:04 -0300

    Add summary metrics

--- index.js
+++ index.js
@@ -24,12 +24,20 @@ const histogram = new promClient.Histogram ({
     buckets: [0.1, 0.2, 0.3, 0.4, 0.5]
 })
 
+const summary = new promClient.Summary ({
+    name: 'request_time_summary_seconds',
+    help: 'Tempo de resposta da API',
+    percentiles: [0.5, 0.9, 0.99]
+})
+
 app.get('/', function(req, res) {
     counter.labels('200').inc();
 
     gauge.set(100*Math.random());
 
-    histogram.observe(Math.random());
+    const tempo = Math.random()
+    histogram.observe(tempo);
+    summary.observe(tempo);
 
     res.send('Hello World!');
 });
